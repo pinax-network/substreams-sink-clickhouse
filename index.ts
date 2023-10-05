148ecc4927d0911c77301f42cc97c4b3e98db117
Julien Rousseau <julrousseau20@gmail.com>
2023-10-05 12:33:05 -0400

    added verbose option

--- index.ts
+++ index.ts
@@ -1,31 +1,44 @@
-import { Command } from "commander";
+import { Command, Option } from "commander";
 import {
   description,
   name,
   version,
 } from "./package.json" assert { type: "json" };
+import { ping } from "./src/commands/ping.js";
+import { executeInitialSchema } from "./src/commands/schema.js";
+import { serveSink } from "./src/commands/serve.js";
 import config from "./src/config.js";
 import { logger } from "./src/logger.js";
-import { ping } from "./src/ping.js";
-import { executeInitialSchema } from "./src/schema.js";
-import { serveSink } from "./src/server.js";
-
-logger.enable();
 
 const program = new Command();
 program.name(name).description(description).version(version);
+
+const verboseOption = new Option(
+  "-v, --verbose [format]",
+  "enable logs. format options: [json, pretty]"
+);
+
+program.hook("preAction", (_, command) => {
+  const options = command.opts();
+  if (options?.verbose) {
+    logger.enable(options.verbose === "json" ? "json" : "pretty");
+  } else {
+    logger.disable();
+  }
+});
 
 program
   .command("run")
   .description(
     "serves the sink on http to receive substreams data from substreams-sink-webhook"
   )
+  .addOption(verboseOption)
   .option("-s, --schema [schema]")
   .action(async (options) => {
     if ("schema" in options) {
-      const schemaPath =
-        typeof options.schema === "string" ? options.schema : config.SCHEMA;
-      await executeInitialSchema(schemaPath);
+      await executeInitialSchema(
+        typeof options.schema === "string" ? options.schema : config.SCHEMA
+      );
     }
 
     serveSink();
