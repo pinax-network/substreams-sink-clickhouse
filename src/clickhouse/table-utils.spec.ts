d99826a37d6a20eb88ec81eb41c5a2f678ccd269
Denis Carriere <carriere.denis@gmail.com>
2023-10-20 21:10:19 -0400

    rename tests

--- src/clickhouse/table-utils.spec.ts
+++ src/clickhouse/table-utils.spec.ts
@@ -1,57 +1,49 @@
-import { describe, expect, test } from "bun:test";
+import { expect, test } from "bun:test";
 import { getTableName, splitSchemaByTableCreation } from "./table-utils.js";
 
-describe("table-utils", () => {
-  describe("splitSchemByTableCreation", () => {
-    test("it should extract the table creation queries", () => {
-      const schema = `CREATE TABLE foo (num UInt64)
-       ENGINE MergeTree
-       ORDER BY (num)
+test("splitSchemaByTableCreation", () => {
+  const schema = `CREATE TABLE foo (num UInt64)
+    ENGINE MergeTree
+    ORDER BY (num)
 
-       CREATE TABLE IF NOT EXISTS bar (str UInt64)
-       ENGINE MergeTree
-       ORDER BY (str)`;
+    CREATE TABLE IF NOT EXISTS bar (str UInt64)
+    ENGINE MergeTree
+    ORDER BY (str)`;
 
-      const tables = splitSchemaByTableCreation(schema);
+  const tables = splitSchemaByTableCreation(schema);
 
-      expect(tables.length).toBe(2);
+  expect(tables.length).toBe(2);
 
-      expect(tables[0].tableName).toBe("foo");
-      expect(tables[1].tableName).toBe("bar");
+  expect(tables[0].tableName).toBe("foo");
+  expect(tables[1].tableName).toBe("bar");
 
-      expect(tables[0].query.includes("CREATE TABLE  foo")).toBeTrue();
-      expect(tables[0].query.includes("ORDER BY (num)")).toBeTrue();
-      expect(
-        tables[1].query.includes("CREATE TABLE  IF NOT EXISTS bar")
-      ).toBeTrue();
-      expect(tables[1].query.includes("ORDER BY (str)")).toBeTrue();
-    });
-  });
+  expect(tables[0].query.includes("CREATE TABLE  foo")).toBeTrue();
+  expect(tables[0].query.includes("ORDER BY (num)")).toBeTrue();
+  expect(tables[1].query.includes("CREATE TABLE  IF NOT EXISTS bar")).toBeTrue();
+  expect(tables[1].query.includes("ORDER BY (str)")).toBeTrue();
+});
 
-  describe("getTableName", () => {
-    test("it should find the table name from the query", () => {
-      const queries = [
-        `CREATE TABLE foo (num UInt64)
-        ENGINE MergeTree
-        ORDER BY (num)`,
+test("getTableName", () => {
+  const queries = [
+    `CREATE TABLE foo (num UInt64)
+    ENGINE MergeTree
+    ORDER BY (num)`,
 
-        `CREATE TABLE db_name.foo (num UInt64)
-        ENGINE MergeTree
-        ORDER BY (num)`,
+    `CREATE TABLE db_name.foo (num UInt64)
+    ENGINE MergeTree
+    ORDER BY (num)`,
 
-        `CREATE TABLE db_name.foo ON shard_1 (str FixedString(12))
-        ENGINE MergeTree
-        ORDER BY (str)`,
+    `CREATE TABLE db_name.foo ON shard_1 (str FixedString(12))
+    ENGINE MergeTree
+    ORDER BY (str)`,
 
-        `create table if not exists foo (num UInt64)
-        engine MergreTree
-        order by (num)`,
-      ];
+    `create table if not exists foo (num UInt64)
+    engine MergreTree
+    order by (num)`,
+  ];
 
-      for (const query of queries) {
-        const tableName = getTableName(query);
-        expect(tableName).toBe("foo");
-      }
-    });
-  });
+  for (const query of queries) {
+    const tableName = getTableName(query);
+    expect(tableName).toBe("foo");
+  }
 });
--- src/entity-changes.spec.ts
+++ src/entity-changes.spec.ts
@@ -1,32 +1,30 @@
 import { EntityChange } from "@substreams/sink-entity-changes/zod";
-import { describe, expect, test } from "bun:test";
+import { expect, test } from "bun:test";
 import { getValuesInEntityChange } from "./entity-changes.js";
 
 // TO-DO: should be moved to @substreams/sink-entity-changes
-describe("entity-changes", () => {
-  test("it should parse fields out of entity-changes correctly", () => {
-    const change: Partial<EntityChange> = {
-      fields: [
-        { name: "value a", newValue: { string: "a string" } },
-        { name: "value b", newValue: { int32: 1 } },
-        { name: "value c", newValue: { bigint: "12345" } },
-        { name: "value d", newValue: { bigdecimal: "1234.5678" } },
-        { name: "value e", newValue: { bool: true } },
-        { name: "value f", newValue: { bytes: "eeff" } },
-        { name: "value g", newValue: { undefined: undefined } },
-        { name: "value h", newValue: { array: [1, 2, 3, 4] } },
-      ],
-    };
+test("getValuesInEntityChange", () => {
+  const change: Partial<EntityChange> = {
+    fields: [
+      { name: "value a", newValue: { string: "a string" } },
+      { name: "value b", newValue: { int32: 1 } },
+      { name: "value c", newValue: { bigint: "12345" } },
+      { name: "value d", newValue: { bigdecimal: "1234.5678" } },
+      { name: "value e", newValue: { bool: true } },
+      { name: "value f", newValue: { bytes: "eeff" } },
+      { name: "value g", newValue: { undefined: undefined } },
+      { name: "value h", newValue: { array: [1, 2, 3, 4] } },
+    ],
+  };
 
-    const values = getValuesInEntityChange(change as EntityChange);
-    expect(values).toEqual({
-      "value a": "a string",
-      "value b": 1,
-      "value c": "12345",
-      "value d": "1234.5678",
-      "value e": true,
-      "value f": "eeff",
-      "value h": "[1, 2, 3, 4]",
-    });
+  const values = getValuesInEntityChange(change as EntityChange);
+  expect(values).toEqual({
+    "value a": "a string",
+    "value b": 1,
+    "value c": "12345",
+    "value d": "1234.5678",
+    "value e": true,
+    "value f": "eeff",
+    "value h": "[1, 2, 3, 4]",
   });
 });
