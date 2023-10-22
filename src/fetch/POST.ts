204759107316109c48d7b826ceed83218f51fa7d
Denis Carriere <carriere.denis@gmail.com>
2023-10-21 22:01:50 -0400

    refactor POST request

--- src/fetch/POST.ts
+++ src/fetch/POST.ts
@@ -1,31 +1,18 @@
 import { BodySchema } from "../schemas.js";
-import { config } from "../config.js";
-import { verify } from "../webhook/verify.js";
 import { handleSinkRequest } from "../clickhouse/handleSinkRequest.js";
+import signatureEd25519 from "./signatureEd25519.js";
 
 export default async function (req: Request) {
-    // const { pathname} = new URL(req.url);
-
-    // if (pathname !== "/webhook" ) return new Response("Not found", { status: 400 });
-
-    const timestamp = req.headers.get("x-signature-timestamp");
-    const signature = req.headers.get("x-signature-ed25519");
+    // validate Ed25519 signature
     const text = await req.text();
+    const signatureError = await signatureEd25519(req, text);
+    if ( signatureError ) return signatureError;
 
-    if (!timestamp) return new Response("missing required timestamp in headers", {status: 400});
-    if (!signature) return new Response("missing required signature in headers", {status: 400 });
-    if (!text) return new Response("missing body", { status: 400 });
-
-    const msg = Buffer.from(timestamp + text);
-    const isVerified = verify(msg, signature, config.publicKey);
-
-    if (!isVerified) return new Response("invalid request signature", { status: 400 });
-
+    // parse POST body payload
     const body = BodySchema.parse(JSON.parse(text));
-
     if ( "message" in body ) {
         if (body.message === "PING") return new Response("OK");
         return new Response("invalid body", { status: 400 });
     }
-    return handleSinkRequest(body)
+    return handleSinkRequest(body);
 }
---
+++ src/fetch/signatureEd25519.ts
@@ -0,0 +1,16 @@
+import { config } from "../config.js";
+import { verify } from "../webhook/verify.js";
+
+export default async function (req: Request, text: string) {
+    const timestamp = req.headers.get("x-signature-timestamp");
+    const signature = req.headers.get("x-signature-ed25519");
+
+    if (!timestamp) return new Response("missing required timestamp in headers", {status: 400});
+    if (!signature) return new Response("missing required signature in headers", {status: 400 });
+    if (!text) return new Response("missing body", { status: 400 });
+
+    const msg = Buffer.from(timestamp + text);
+    const isVerified = verify(msg, signature, config.publicKey);
+
+    if (!isVerified) return new Response("invalid request signature", { status: 401 });
+}
