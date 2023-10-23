50eeac871f57bff3e58a655430935070e3bbc737
Julien Rousseau <julrousseau20@gmail.com>
2023-10-23 10:29:39 -0400

    fixed linting error

--- src/fetch/PUT.ts
+++ src/fetch/PUT.ts
@@ -2,10 +2,10 @@ import schema from "./schema.js";
 import init from "./init.js";
 import { validateBearerAuth } from "./bearerAuth.js";
 
-export default async function (req: Request) {
+export default async function (req: Request):Promise<Response> {
     const { pathname} = new URL(req.url);
     const authError = validateBearerAuth(req);
-    if ( authError ) return authError;
+    if ( authError instanceof Response ) return authError;
 
     if ( pathname === "/schema") return schema(req);
     if ( pathname === "/init") return init(req);
