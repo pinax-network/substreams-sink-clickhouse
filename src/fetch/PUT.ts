dfb0f215a6e9ad667b3dc799a0024b1d4c85d882
Julien Rousseau <julrousseau20@gmail.com>
2023-10-23 13:29:19 -0400

    added graphql conversion skeleton

--- src/fetch/PUT.ts
+++ src/fetch/PUT.ts
@@ -1,14 +1,17 @@
 import { validateBearerAuth } from "./bearerAuth.js";
 import init from "./init.js";
-import schema from "./schema.js";
+import { handleSchemaRequest } from "./schema.js";
 
 export default async function (req: Request): Promise<Response> {
   const { pathname } = new URL(req.url);
   const authError = validateBearerAuth(req);
-  if (authError instanceof Response) return authError;
+  if (authError instanceof Response) {
+    return authError;
+  }
 
-  if (pathname === "/schema/sql") return schema(req);
-  if (pathname === "/schema/graphql") return new Response("Not implemented", { status: 501 });
   if (pathname === "/init") return init();
+  if (pathname === "/schema/sql") return handleSchemaRequest(req, "sql");
+  if (pathname === "/schema/graphql") return handleSchemaRequest(req, "graphql");
+
   return new Response("Not found", { status: 400 });
 }
--- src/fetch/schema.ts
+++ src/fetch/schema.ts
@@ -1,7 +1,7 @@
 import { initializeTables } from "../clickhouse/table-initialization.js";
 import { TableInitSchema } from "../schemas.js";
 
-export default async function (req: Request) {
+export async function handleSchemaRequest(req: Request, type: "sql" | "graphql") {
   const body = await req.text();
   if (!body) {
     return new Response("missing body", { status: 400 });
@@ -12,9 +12,17 @@ export default async function (req: Request) {
     return new Response(result.error.toString(), { status: 400 });
   }
 
+  let schema = result.data;
+  if (type === "graphql") {
+    // TODO: import graphql -> sql code
+    return new Response("Not implemented", { status: 501 });
+  }
+
   try {
-    await initializeTables(result.data);
-    return new Response("OK");
+    await initializeTables(schema);
+    return new Response(JSON.stringify({ status: "OK", schema }), {
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (err) {
     return new Response(`Could not create the tables: ${err}`, { status: 500 });
   }
