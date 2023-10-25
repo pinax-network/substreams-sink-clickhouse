83ab8a1f5dbbdf40081a930a84b00ea23d019d18
Denis Carriere <carriere.denis@gmail.com>
2023-10-25 12:02:56 -0400

    refactor swagger imports

--- src/fetch/GET.ts
+++ src/fetch/GET.ts
@@ -1,23 +1,18 @@
-import favicon from "../../swagger/favicon.png";
-import swaggerUI from "../../swagger/index.html";
-
 import { file } from "bun";
 import { registry } from "../prometheus.js";
 import health from "./health.js";
 import openapi from "./openapi.js";
+import swaggerHtml from "../../swagger/index.html"
+import swaggerFavicon from "../../swagger/favicon.png"
 
 export default async function (req: Request) {
   const { pathname } = new URL(req.url);
 
-  if (pathname === "/") return new Response(file(swaggerUI));
-  if (pathname === "/favicon.png") return new Response(file(favicon));
+  if (pathname === "/") return new Response(file(swaggerHtml));
+  if (pathname === "/favicon.png") return new Response(file(swaggerFavicon));
   if (pathname === "/health") return health(req);
-  if (pathname === "/metrics")
-    return new Response(await registry.metrics(), {
-      headers: { "Content-Type": registry.contentType },
-    });
-  if (pathname === "/openapi")
-    return new Response(openapi, { headers: { "Content-Type": "application/json" } });
+  if (pathname === "/metrics") return new Response(await registry.metrics(), { headers: { "Content-Type": registry.contentType }});
+  if (pathname === "/openapi") return new Response(openapi, { headers: { "Content-Type": "application/json" } });
 
   return new Response("Not found", { status: 400 });
 }
