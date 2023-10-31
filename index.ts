1f363fe24ebec2b82eb97bf0a254854213ae1174
Julien Rousseau <julrousseau20@gmail.com>
2023-10-31 09:35:55 -0400

    added cors

--- index.ts
+++ index.ts
@@ -2,8 +2,10 @@
 
 import { config } from "./src/config.js";
 import GET from "./src/fetch/GET.js";
+import OPTIONS from "./src/fetch/OPTIONS.js";
 import POST from "./src/fetch/POST.js";
 import PUT from "./src/fetch/PUT.js";
+import { NotFound } from "./src/fetch/cors.js";
 import { logger } from "./src/logger.js";
 
 if (config.verbose) logger.enable();
@@ -12,10 +14,11 @@ const app = Bun.serve({
   hostname: config.hostname,
   port: config.port,
   fetch(req: Request) {
-    if (req.method == "GET") return GET(req);
-    if (req.method == "POST") return POST(req);
-    if (req.method == "PUT") return PUT(req);
-    return new Response("Invalid request", { status: 400 });
+    if (req.method === "GET") return GET(req);
+    if (req.method === "POST") return POST(req);
+    if (req.method === "PUT") return PUT(req);
+    if (req.method === "OPTIONS") return OPTIONS(req);
+    return NotFound;
   },
 });
 
