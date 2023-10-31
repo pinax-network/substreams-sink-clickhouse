02d9c32b16682def759bb21d99f7bd9e0e3d84ff
Julien Rousseau <julrousseau20@gmail.com>
2023-10-31 17:17:38 -0400

    updated missing query to joins

--- src/fetch/cursors.ts
+++ src/fetch/cursors.ts
@@ -1,5 +1,7 @@
 import { readOnlyClient } from "../clickhouse/createClient.js";
 import { store } from "../clickhouse/stores.js";
+import { logger } from "../logger.js";
+import { BadRequest, toJSON, toText } from "./cors.js";
 
 export async function findLatestCursor(req: Request): Promise<Response> {
   const parametersResult = await verifyParameters(req);
@@ -23,16 +25,13 @@ export async function findLatestCursor(req: Request): Promise<Response> {
     const data = await response.json<Array<unknown>>();
 
     if (data.length === 1) {
-      return new Response(JSON.stringify(data[0]), {
-        headers: { "Content-Type": "application/json" },
-      });
+      return toJSON(data[0]);
     }
 
-    return new Response(`Bad request: no cursor found for '${table}' on '${chain}'.`, {
-      status: 400,
-    });
+    return toText(`Bad request: no cursor found for '${table}' on '${chain}'.`, 400);
   } catch (err) {
-    return new Response("Bad request", { status: 400 });
+    logger.error(err);
+    return BadRequest;
   }
 }
 
@@ -45,24 +44,33 @@ export async function findCursorsForMissingBlocks(req: Request): Promise<Respons
   try {
     const { table, chain } = parametersResult;
 
+    // This query finds every block that does not have a next block (beginning of a missing range).
+    // It then pairs it with the first existing block after it (end of missing range).
+    // If the start block is the last one in the database (max value), it is ignored.
+    // When every range is found, they are joined with the specified ${table} and cursors,
+    // to find which cursor is associated with the min and max boundaries.
     const query = `
-SELECT from AS from_block_number, c1.cursor AS from_cursor, to AS to_block_number, c2.cursor AS to_cursor
-FROM blocks b1, blocks b2, ${table} t1, ${table} t2, cursors c1, cursors c2 (
+SELECT from AS from_block_number, c1.cursor AS from_cursor, to AS to_block_number, c2.cursor AS to_cursor 
+FROM ( 
     SELECT b1.block_number AS from, MIN(b2.block_number) AS to
     FROM blocks b1, blocks b2, (
         SELECT MAX(block_number) AS block_number
         FROM blocks
         WHERE chain = '${chain}'
-    ) maximum
+    ) AS maximum
     WHERE b1.block_number + 1 NOT IN (SELECT block_number FROM blocks WHERE chain = '${chain}')
         AND b1.block_number <> maximum.block_number
         AND b1.chain = '${chain}'
         AND b1.chain = '${chain}'
         AND b2.block_number > b1.block_number
-    GROUP BY b1.block_number) AS block_ranges,
-WHERE b1.block_number = block_ranges.FROM AND b2.block_number = block_ranges.to AND b1.chain = '${chain}' and b2.chain = '${chain}'
-    AND t1.block_id = b1.block_id AND t2.block_id = b2.block_id AND t1.chain = '${table}' AND t2.chain = '${chain}'
-    AND c1.block_id = b1.block_id AND c2.block_id = b2.block_id AND c1.chain = '${table}' AND c2.chain = '${chain}'`;
+    GROUP BY b1.block_number 
+) AS block_ranges
+JOIN blocks   b1 ON b1.block_number = block_ranges.from AND b1.chain = '${chain}'
+JOIN blocks   b2 ON b2.block_number = block_ranges.to   AND b2.chain = '${chain}'
+JOIN ${table} t1 ON t1.block_id = b1.block_id           AND t1.chain = '${chain}'
+JOIN ${table} t2 ON t2.block_id = b2.block_id           AND t2.chain = '${chain}'
+JOIN cursors  c1 ON c1.block_id = t1.block_id           AND c1.chain = '${chain}'
+JOIN cursors  c2 ON c2.block_id = t2.block_id           AND c2.chain = '${chain}'`;
 
     const response = await readOnlyClient.query({ query, format: "JSONEachRow" });
     const data = await response.json<
@@ -79,12 +87,10 @@ WHERE b1.block_number = block_ranges.FROM AND b2.block_number = block_ranges.to 
       to: { block: record.to_block_number, cursor: record.to_cursor },
     }));
 
-    return new Response(JSON.stringify(dto), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
-  } catch {
-    return new Response("Bad request", { status: 400 });
+    return toJSON(dto);
+  } catch (err) {
+    logger.error(err);
+    return BadRequest;
   }
 }
 
@@ -96,19 +102,19 @@ async function verifyParameters(
   const table = url.searchParams.get("table");
 
   if (!chain) {
-    return new Response("Missing parameter: chain", { status: 400 });
+    return toText("Missing parameter: chain", 400);
   }
 
   if (!table) {
-    return new Response("Missing parameter: table", { status: 400 });
+    return toText("Missing parameter: table", 400);
   }
 
   if (!(await store.chains).includes(chain)) {
-    return new Response("Invalid parameter: chain=" + chain, { status: 400 });
+    return toText("Invalid parameter: chain=" + chain, 400);
   }
 
   if (!(await store.publicTables).includes(table)) {
-    return new Response("Invalid parameter: table=" + table, { status: 400 });
+    return toText("Invalid parameter: table=" + table, 400);
   }
 
   return { chain, table };
