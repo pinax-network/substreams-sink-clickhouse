6f72613e82e76fd2e2a8729d690b9f93c63e5d2f
Julien Rousseau <julrousseau20@gmail.com>
2023-11-27 15:27:22 -0500

    implemented cached signatures

--- src/webhook/signatureEd25519.ts
+++ src/webhook/signatureEd25519.ts
@@ -1,17 +1,20 @@
 import { config } from "../config.js";
 import { toText } from "../fetch/cors.js";
-import { verify } from "./verify.js";
+import { Err, Ok, Result } from "../result.js";
+import { CachedVerifier } from "./cached-verifier.js";
 
-export default async function (req: Request, text: string) {
-  const timestamp = req.headers.get("x-signature-timestamp");
+let verifier: CachedVerifier | null = null;
+
+export default async function (req: Request, text: string): Promise<Result<undefined, Response>> {
   const signature = req.headers.get("x-signature-ed25519");
 
-  if (!timestamp) return toText("missing required timestamp in headers", 400);
-  if (!signature) return toText("missing required signature in headers", 400);
-  if (!text) return toText("missing body", 400);
+  if (!signature) return Err(toText("missing required signature in headers", 400));
+  if (!text) return Err(toText("missing body", 400));
 
-  const msg = Buffer.from(timestamp + text);
-  const isVerified = verify(msg, signature, config.publicKey);
+  verifier ??= new CachedVerifier([config.publicKey]);
+  const isVerified = verifier.verify(signature);
 
-  if (!isVerified) return toText("invalid request signature", 401);
+  if (!isVerified) return Err(toText("invalid request signature", 401));
+
+  return Ok();
 }
