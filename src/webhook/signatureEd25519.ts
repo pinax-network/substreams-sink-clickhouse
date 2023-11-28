2f3d3847582be048262c6e97f2fe0d5a414c4fb1
Julien Rousseau <julrousseau20@gmail.com>
2023-11-28 12:47:37 -0500

    updated to auth process with headers

--- src/webhook/signatureEd25519.ts
+++ src/webhook/signatureEd25519.ts
@@ -1,14 +1,23 @@
+import { config } from "../config.js";
 import { toText } from "../fetch/cors.js";
 import { Err, Ok, Result } from "../result.js";
-import { verify } from "./verify.js";
+import { cachedVerify } from "./temp.js";
 
 export default async function (req: Request, text: string): Promise<Result<undefined, Response>> {
   const signature = req.headers.get("x-signature-ed25519");
+  const expiry = req.headers.get("x-signature-ed25519-expiry");
+  const publicKey = req.headers.get("x-signature-ed25519-public-key");
 
   if (!signature) return Err(toText("missing required signature in headers", 400));
+  if (!expiry) return Err(toText("missing required expiry in headers", 400));
+  if (!publicKey) return Err(toText("missing required public key in headers", 400));
   if (!text) return Err(toText("missing body", 400));
 
-  if (!verify(signature)) {
+  if (!config.publicKey.includes(publicKey)) {
+    return Err(toText("invalid public key", 401));
+  }
+
+  if (!cachedVerify(signature, Number(expiry), publicKey)) {
     return Err(toText("invalid request signature", 401));
   }
 
