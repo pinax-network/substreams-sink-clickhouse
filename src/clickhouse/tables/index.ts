39d758762486b206fe20b1bb43082ad042669fd6
Julien Rousseau <julrousseau20@gmail.com>
2023-11-28 14:20:30 -0500

    removed cursors table

--- src/clickhouse/tables/index.ts
+++ src/clickhouse/tables/index.ts
@@ -1,5 +1,4 @@
 import blocks_sql from "./blocks.sql";
-import cursors_sql from "./cursors.sql";
 import deleted_entity_changes_sql from "./deleted_entity_changes.sql";
 import final_blocks_sql from "./final_blocks.sql";
 import module_hashes_sql from "./module_hashes.sql";
@@ -9,7 +8,6 @@ export const blocks = await Bun.file(blocks_sql).text();
 export const final_blocks = await Bun.file(final_blocks_sql).text();
 export const module_hashes = await Bun.file(module_hashes_sql).text();
 export const unparsed_json = await Bun.file(unparsed_json_sql).text();
-export const cursors = await Bun.file(cursors_sql).text();
 export const deleted_entity_changes = await Bun.file(deleted_entity_changes_sql).text();
 
 export default [
@@ -17,6 +15,5 @@ export default [
   ["final_blocks", final_blocks],
   ["module_hashes", module_hashes],
   ["unparsed_json", unparsed_json],
-  ["cursors", cursors],
   ["deleted_entity_changes", deleted_entity_changes],
 ];
