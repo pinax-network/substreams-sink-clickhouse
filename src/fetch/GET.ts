7a3f645d83a1ccb8bc94e5cd156ef1898896d9e3
Julien Rousseau <julrousseau20@gmail.com>
2023-11-28 14:46:11 -0500

    removed missing cursors query

--- src/fetch/GET.ts
+++ src/fetch/GET.ts
@@ -4,7 +4,7 @@ import swaggerHtml from "../../swagger/index.html";
 import { metrics } from "../prometheus.js";
 import { blocks } from "./blocks.js";
 import { NotFound, toFile, toJSON } from "./cors.js";
-import { findCursorsForMissingBlocks, findLatestCursor } from "./cursors.js";
+import { findLatestCursor } from "./cursors.js";
 import health from "./health.js";
 import { openapi } from "./openapi.js";
 
@@ -18,7 +18,6 @@ export default async function (req: Request) {
   if (pathname === "/openapi") return toJSON(await openapi());
   if (pathname === "/blocks") return blocks();
   if (pathname === "/cursors/latest") return findLatestCursor(req);
-  if (pathname === "/cursors/missing") return findCursorsForMissingBlocks(req);
 
   return NotFound;
 }
